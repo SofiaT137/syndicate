2be859e00cf679bbcc3ea913ca1291d0dc245e9a
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2024-05-27 12:30:15 +0300

    feat: Return task04/ folder

---
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -0,0 +1,31 @@
+package com.task04;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.ResourceType;
+import com.syndicate.deployment.model.RetentionSetting;
+
+
+@LambdaHandler(lambdaName = "sqs_handler",
+		roleName = "sqs_handler-role",
+		isPublishVersion = true,
+		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@SqsTriggerEventSource(
+		targetQueue = "async_queue",
+		batchSize = 10
+)
+@DependsOn(
+		name = "async_queue",
+		resourceType = ResourceType.SQS_QUEUE
+)
+public class SqsHandler implements RequestHandler<Object, String> {
+
+	public String handleRequest(Object request, Context context) {
+		System.out.println(request.toString());
+		return "";
+	}
+}
