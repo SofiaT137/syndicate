a8b9c2e4e71229718ab12722b90450adc3b649a3
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2024-06-02 00:04:16 +0300

    fix: Fix task08/

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -11,7 +11,6 @@ import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -21,15 +20,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(lambdaName = "api_handler",
-	roleName = "api_handler-role",
-	layers = {"sdk-layer"},
-	runtime = DeploymentRuntime.JAVA11,
-	architecture = Architecture.ARM64,
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+		roleName = "api_handler-role",
+		layers = {"sdk-layer"},
+		runtime = DeploymentRuntime.JAVA11,
+		architecture = Architecture.ARM64,
+		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @LambdaLayer(
 		layerName = "sdk-layer",
-		libraries = {"lib/commons-lang3-3.14.0.jar", "lib/gson-2.10.1.jar"},
+		libraries = {"lib/open_meteo_api-0.0.1.jar"},
 		runtime = DeploymentRuntime.JAVA11,
 		architectures = {Architecture.ARM64},
 		artifactExtension = ArtifactExtension.ZIP
@@ -39,18 +38,12 @@ import java.util.Map;
 		invokeMode = InvokeMode.BUFFERED
 )
 public class ApiHandler implements RequestHandler<Object, String> {
-	private static final HttpClient httpClient = HttpClient.newHttpClient();
-	private static final String OPEN_METEO_API_URL = "https://api.open-meteo.com/v1/forecast?latitude=40.1792&longitude=44.4991&hourly=temperature_2m";
-
 	public String handleRequest(Object request, Context context) {
 		Map<String, Object> result = new HashMap<>();
 		try {
-			HttpRequest httpRequest = HttpRequest.newBuilder()
-					.GET()
-					.uri(URI.create(OPEN_METEO_API_URL))
-					.build();
-			HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-			return httpResponse.body().replaceAll("\\\"", "\"");
+			var weatherService = new WeatherService();
+			var resultString = weatherService.getWeatherForecast();
+			return resultString.replaceAll("\\\"", "\"");
 		} catch (IOException | InterruptedException exception) {
 			result.put("statusCode", 400);
 			result.put("body", exception.getMessage());
