83621f9996289065bec4bc709c176bb50e44111b
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2024-06-02 00:44:54 +0300

    fix: Fix task05

--- task05/jsrc/main/java/com/task05/ApiHandler.java
+++ task05/jsrc/main/java/com/task05/ApiHandler.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.xspec.S;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
@@ -22,23 +23,15 @@ import java.util.UUID;
 		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 public class ApiHandler implements RequestHandler<Request, Map<String, Object>> {
-
-	private static final AmazonDynamoDB client = AmazonDynamoDBClientBuilder.defaultClient();
-	private final DynamoDB dynamoDB = new DynamoDB(client);
+	private static final DynamoDB db = DynamoDBClient.getInstance();
+	private static final String DYNAMODB_NAME = "cmtr-401608dd-Events-test";
 
 	public Map<String, Object> handleRequest(Request request, Context context) {
 		try {
-			UUID uuid = UUID.randomUUID();
-			Item item = new Item();
-			var principal = request.getPrincipalId();
-			item.withString("id", uuid.toString());
-			item.withInt("principalId", principal);
-			var content =  request.getContent();
-			item.withMap("body", content);
-			context.getLogger().log("Content: " + content);
-			item.withString("createdAt", ZonedDateTime.now( ZoneOffset.UTC ).format( DateTimeFormatter.ISO_INSTANT ));
+			var uuid = getRandomUUID();
+			var item = getItem(request, uuid);
 			var result = item.asMap();
-			Table table = dynamoDB.getTable("cmtr-401608dd-Events-test");
+			Table table = db.getTable(DYNAMODB_NAME);
 			table.putItem(item);
 			Map<String, Object> resultMap = new HashMap<>();
 			resultMap.put("statusCode", 201);
@@ -49,5 +42,21 @@ public class ApiHandler implements RequestHandler<Request, Map<String, Object>> 
 			context.getLogger().log(e.getMessage());
 			throw new RuntimeException(e);
 		}
+	}
+
+	private UUID getRandomUUID() {
+		return UUID.randomUUID();
+	}
+
+	private Item getItem(Request request, UUID uuid) {
+		Item item = new Item();
+		var principal = request.getPrincipalId();
+		item.withString("id", uuid.toString());
+		item.withInt("principalId", principal);
+		var content =  request.getContent();
+		item.withMap("body", content);
+		item.withString("createdAt", ZonedDateTime.now( ZoneOffset.UTC )
+				.format( DateTimeFormatter.ISO_INSTANT));
+		return item;
 	}
 }
