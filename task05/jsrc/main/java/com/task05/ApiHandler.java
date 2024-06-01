2c3d1807bc65231c30bf5e053b37215f51846aca
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2024-06-02 00:48:27 +0300

    fix: Fix task05

--- task05/jsrc/main/java/com/task05/ApiHandler.java
+++ task05/jsrc/main/java/com/task05/ApiHandler.java
@@ -23,7 +23,7 @@ import java.util.UUID;
 		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 public class ApiHandler implements RequestHandler<Request, Map<String, Object>> {
-	private static final DynamoDB db = DynamoDBClient.getInstance();
+	private static final DynamoDB DYNAMO_DB = DynamoDBClient.getInstance();
 	private static final String DYNAMODB_NAME = "cmtr-401608dd-Events-test";
 
 	public Map<String, Object> handleRequest(Request request, Context context) {
@@ -31,7 +31,7 @@ public class ApiHandler implements RequestHandler<Request, Map<String, Object>> 
 			var uuid = getRandomUUID();
 			var item = getItem(request, uuid);
 			var result = item.asMap();
-			Table table = db.getTable(DYNAMODB_NAME);
+			Table table = DYNAMO_DB.getTable(DYNAMODB_NAME);
 			table.putItem(item);
 			Map<String, Object> resultMap = new HashMap<>();
 			resultMap.put("statusCode", 201);
