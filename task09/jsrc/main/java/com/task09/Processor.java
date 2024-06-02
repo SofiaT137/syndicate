dfa7d712726164716e17bed3903b0034f644e718
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2024-06-03 00:00:20 +0300

    fix: Add task09

--- task09/jsrc/main/java/com/task09/Processor.java
+++ task09/jsrc/main/java/com/task09/Processor.java
@@ -24,6 +24,7 @@ import java.util.UUID;
 @LambdaHandler(lambdaName = "processor",
 	roleName = "processor-role",
 	isPublishVersion = false,
+	layers = {"sdk-layer"},
 	runtime = DeploymentRuntime.JAVA11,
 	architecture = Architecture.ARM64,
 	tracingMode = TracingMode.Active,
